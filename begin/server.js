517875df3eecaa9d0df8cdc4a4d136eda69e49c3
doortts <doortts@gmail.com>
2011-12-21 19:21:55 +0900

    debugging...

--- begin/server.js
+++ begin/server.js
@@ -11,11 +11,12 @@ var url = require("url");
 function start(route, handle) {
     function onRequest(request, response) {
         var pathname = url.parse(request.url).pathname;
-        console.log("Request for " + pathname + " received.");
-        route(handle, pathname);
+        console.log("Request for [ " + pathname + " ] received.");
 
         response.writeHead(200, {"Content-Type":"text/plain"});
-        response.write("Hello World~!");
+        var content = route(handle, pathname);
+        console.log(">>>" + content);
+        response.write(content);
         response.end();
     }
     http.createServer(onRequest).listen(8888);
